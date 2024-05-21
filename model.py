c255e4e53f8f31c79815fa66ef3136d7130845c4
shikhar maurya <shikharyashmaurya@users.noreply.huggingface.co>
2024-05-21 10:30:12 +0000

    Delete model.py

--- model.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-data = pd.read_csv('D:\Downloads\Campus-Placement-Prediction-Using-Machine-Learning-main\Campus-Placement-Prediction-Using-Machine-Learning-main\Placement_Data_Full_Class (1).csv')
-import warnings
-warnings.filterwarnings('ignore')
